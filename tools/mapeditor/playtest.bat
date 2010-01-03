9079cb80e531b02e6f4a2bb79bbc844c5dd7a105
Andrew Kelley <superjoe30@gmail.com>
2010-01-02 22:28:39 -0700

    New map editor
    * Removed old map editor written in VB6
    * Started new map editor written in C++ and Qt

--- tools/mapeditor/playtest.bat
+++
@@ -1,4 +0,0 @@
-@echo on
-cd ..\..
-call make.bat playtest
-motrs.exe %*
---
+++ tools/world-editor/mainwindow.cpp
@@ -0,0 +1,13 @@
+#include "mainwindow.moc"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent), ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
