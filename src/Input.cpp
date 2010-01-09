d9e7b88d4f7eb8a30bc2c4ca879541d241d3cb4f
Josh Wolfe <thejoshwolfe@debian.lan>
2010-01-09 00:35:02 -0700

    Input is ready for testing

--- src/Input.cpp
+++ src/Input.cpp
@@ -1,23 +1,36 @@
 #include "Input.h"
 
-bool Input::keyState(InputKey key) {
-    return false;
+#include "Debug.h"
+#include "Utils.h"
+
+bool Input::m_boolState[] = {false};
+SDLKey Input::m_boolMap[] = {SDLK_UNKNOWN};
+
+
+bool Input::boolState(Key key) {
+    return m_boolState[key];
 }
 
-const std::string Input::description(InputKey key) {
+const std::string Input::description(Key key) {
     switch (key) {
         case Up: return "Up";
         case Right: return "Right";
         case Down: return "Down";
         case Left: return "Left";
-        case Attack1: return "Attack1";
-        case Attack2: return "Attack2";
+        case Attack1: return "Attack 1";
+        case Attack2: return "Attack 2";
         case Jump: return "Jump/Dive";
         case ToggleRun: return "Toggle Run";
-        default: throw 0;
+        default: Debug::assert(false, Utils::intToString(key));
     }
 }
 
-
-
-
+void Input::refresh() {
+    m_boolMap[Up] = SDLK_w;
+    m_boolMap[Right] = SDLK_d;
+    m_boolMap[Down] = SDLK_s;
+    m_boolMap[Left] = SDLK_a;
+    Uint8 * keystate = SDL_GetKeyState(NULL);
+    for (int k = 0; k < Key_size; k++)
+        m_boolState[k] = keystate[m_boolMap[k]];
+}
--- src/Input.h
+++ src/Input.h
@@ -3,18 +3,27 @@
 
 #include <string>
 
+#include "SDL.h"
+
 class Input {
 public:
-    enum InputKey {
+    enum Key {
         // movement
         Up, Right, Down, Left,
         // Actions
         Attack1, Attack2, Jump, ToggleRun,
+
+        Key_size
     };
 
-    static bool keyState(InputKey key);
+    static bool boolState(Key key);
 
-    static const std::string description(InputKey key);
+    static void refresh();
+
+    static const std::string description(Key key);
+private:
+    static bool m_boolState[Key_size];
+    static SDLKey m_boolMap[Key_size];
 };
 
 #endif
