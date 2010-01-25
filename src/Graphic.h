df2c0a5135a902d5cd2f0c7a048213305aea48a3
Josh Wolfe <thejoshwolfe@gmail.com>
2010-01-24 19:21:52 -0700

    cleaning up andy's code

--- src/Graphic.h
+++ src/Graphic.h
@@ -9,62 +9,62 @@
  */
 class Graphic
 {
-    public:
-        // create an animation or bitmap from memory
-        Graphic(const char * buffer);
+public:
+    // create an animation or bitmap from memory
+    Graphic(const char * buffer);
 
-        // create an animation from memory and offset the animation
-        Graphic(const char * buffer, int offset);
-        ~Graphic();
+    // create an animation from memory and offset the animation
+    Graphic(const char * buffer, int offset);
+    ~Graphic();
 
-        // draw to an SDL surface
-        void draw(SDL_Surface * dest, SDL_Rect * destRect);
-        void draw(SDL_Surface * dest, int x, int y);
+    // draw to an SDL surface
+    void draw(SDL_Surface * dest, SDL_Rect * destRect);
+    void draw(SDL_Surface * dest, int x, int y);
 
-        // check if the animation loaded ok
-        bool isGood();
+    // check if the animation loaded ok
+    bool isGood();
 
-    private:
-        /*  storage format:
-            Uint32 GraphicType
-            Uint32 StorageType
-            RGB colorKey
-            Header
-            Image data
-        */
+private:
+    /*  storage format:
+        Uint32 GraphicType
+        Uint32 StorageType
+        RGB colorKey
+        Header
+        Image data
+    */
 
-        enum GraphicType {
-            gtAnimation = 0, // spritesheet
-            gtImage = 1, // still frame
-        };
+    enum GraphicType {
+        gtAnimation = 0, // spritesheet
+        gtImage = 1, // still frame
+    };
 
-        enum StorageType {
-            stBMP = 0,
-            stPNG = 1,
-        };
+    enum StorageType {
+        stBMP = 0,
+        stPNG = 1,
+    };
 
-        typedef struct {
-            Uint8 r;
-            Uint8 g;
-            Uint8 b;
-        } RGB;
+    typedef struct {
+        Uint8 r;
+        Uint8 g;
+        Uint8 b;
+    } RGB;
 
-        typedef struct {
-            Uint32 frameCount;
-            Uint32 framesPerSecond;
-            Uint32 frameWidth;
-            Uint32 frameHeight;
-            Uint32 imageSize; // size in bytes of the following image file
-        } Header;
+    typedef struct {
+        Uint32 frameCount;
+        Uint32 framesPerSecond;
+        Uint32 frameWidth;
+        Uint32 frameHeight;
+        Uint32 imageSize; // size in bytes of the following image file
+    } Header;
 
-        SDL_Surface * m_spriteSheet;
-        int m_frameCount;
-        int m_fps;
-        int m_offset;
+    SDL_Surface * m_spriteSheet;
+    int m_frameCount;
+    int m_fps;
+    int m_offset;
 
-        std::vector<SDL_Rect> m_spriteBounds; // boundaries of each sprite
+    std::vector<SDL_Rect> m_spriteBounds; // boundaries of each sprite
 
-        int currentFrame();
+    int currentFrame();
 };
 
 #endif
